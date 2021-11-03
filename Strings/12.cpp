c1dc40e480e92ec30a5884c9e2088562a9bb0d54
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2021-11-03 16:50:03 +0500

    Add files via upload

---
+++ Strings/12.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string newString = "";
+    string Astr = "";
+
+    cin >> newString;
+
+    for (auto c : newString) {
+
+        Astr += c;
+        if (islower(c)) {
+            Astr = Astr.substr(0, Astr.size() - 1);
+            char x = c-32;
+            Astr += x;
+        }
+    }
+
+    cout << Astr << endl;
+}
+
+
---
+++ Strings/14.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string inputString;
+    string word;
+    string masWords[255];
+
+    cout << "Input sentence:" << endl;
+	getline(cin, inputString);
+    cout << endl;
+      
+    int count = 0;
+    for (auto symb : inputString) {
+        if (symb != ' ')
+            word += symb;
+        else
+        {
+            masWords[count] = word;
+            word = "";
+            ++count;
+        }
+    }
+
+    masWords[count] = word;
+    ++count;
+
+    for (int i = 0; i < count; ++i) {
+
+        for (int j = 0; j < count-i-1; ++j) {
+
+            if (masWords[j].length() < masWords[j + 1].length()) {
+                swap(masWords[j], masWords[j + 1]);
+            }
+        }
+    }
+
+    cout << "Output sentence:" << endl;
+    for (int i = 0; i < count; ++i) {
+        cout << masWords[i] << " ";
+    }
+    cout << endl;
+}
