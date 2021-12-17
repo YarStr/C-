0aab34105bb3612b0d5554c395afb4c20d4761e6
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2021-12-17 23:06:40 +0500

    Create 0267.cpp

---
+++ Sem_9/0267.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n, x, y, min1, max1;
+    double k, v1, v2;
+  
+    cin >> n >> x >> y;
+  
+    min1 = min(x, y);
+    max1 = max(x, y);
+  
+    v1 = 1. / min1;
+    v2 = 1. / max1;
+  
+    if (n > min1) {
+        k = min1 + (n - 1) / (v1 + v2) + 1;
+        cout << (int)k << endl;
+    }
+    else {
+        cout << min1 << endl;
+    }
+}
