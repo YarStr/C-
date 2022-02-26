33071d6b30ae94e156286036db3a3f75a2e24237
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2022-02-26 10:35:22 +0500

    Create Task2.cpp

---
+++ Structures/Task2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student
+{
+    int age;
+    string name;
+    string drink;
+};
+
+int main()
+{
+    Student mas[3];
+    
+    mas[0] = { 23, "John", "Coca-Cola" };
+    mas[1] = { 18, "Mary", "Tea" };
+    mas[2] = { 19, "Alice", "Vodka" };
+
+    int indicator = 0;
+    int maxAge = mas[0].age;
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        if (mas[i].age > maxAge)
+        {
+            maxAge = mas[i].age;
+            indicator = i;
+        }
+    };
+    
+    cout << "Максимальный возраст среди студентов: " << maxAge << endl;
+   
+    return 0;
+}
