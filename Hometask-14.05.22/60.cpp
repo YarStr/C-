02eececa7a0b77db8a1e80bf690f330109138ab9
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2022-05-13 23:20:28 +0500

    Update 60.cpp

--- Hometask-14.05.22/60.cpp
+++ Hometask-14.05.22/60.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
+ 
 using namespace std;
-
-bool isPrime(long long n) {
-	for (long long i = 2; i < sqrt(n) + 1; ++)
-		if (n % i == 0)
-			return false;
-	return true;
+ 
+bool isPrime(int x) {
+    for (int i = 2; i <= sqrt(x); ++i)
+        if (x % i == 0)
+            return false;
+    return true;
 }
-
+ 
 int main()
 {
-	ifstream fin("input.txt");
-	ofstream fout("output.txt");
-
-	int k;
-	fin >> k;
-
-	int count = 0;
-	int num = 2;
-
-	vector <int> vec;
-	while (count < 3571)
-	{
-		if (isPrime(num) == true)
-		{
-			++count;
-			vec.push_back(num);
-		}
-		num++;
-	}
-
-	fout << vec[vec[k - 1] - 1];
-
-	return 0;
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+ 
+    int k;
+    fin >> k;
+ 
+    int num = 2;
+    int ord = 0;
+    int count = 1;
+ 
+    while (ord != k)
+    {
+        ++num;
+        if (isPrime(num) == true)
+        {
+            ++count;
+            if (isPrime(count))
+                ++ord;
+        }
+    }
+ 
+    fout << num;
+ 
+    return 0;
 }
