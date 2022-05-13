ef9893337d087d58c333181314a3b2fc1bef69c8
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2022-05-13 23:26:27 +0500

    Update 241.cpp

--- Hometask-14.05.22/241.cpp
+++ Hometask-14.05.22/241.cpp
@@ -1,24 +1,26 @@
-
 #include <fstream>
 
 using namespace std;
-int main() { 
-	int mas[50] = { 3, 4, 5, 6, 9, 
-                 10, 11, 12, 13, 16,
-                 17, 18, 19, 20, 23,
-                 24, 25, 26, 27, 30,
-                 31, 32, 33, 34, 37,
-                 38, 39, 40, 41, 44,
-                 45, 46, 47, 48, 51,
-                 52, 54, 55, 58, 59,
-                 60, 61, 62, 65, 67,
-                 68, 69, 72, 73, 74 };
-  
-	ifstream fin("input.txt");
-	ofstream fout("output.txt");
-  
-	int k;
-	fin >> k; 
-  
-	fout << mas[k - 1]; 
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    int k;
+    fin >> k;
+
+    int answ = 2 + k % 5 + 7 * (k / 5);
+
+    if (k > 36)
+        answ++;
+
+    if (k > 44)
+        answ++;
+
+    if (answ == 56 || answ == 63 || answ == 70 || answ == 71)
+        answ += 2;
+
+    fout << answ;
+
+    return 0;
 }
