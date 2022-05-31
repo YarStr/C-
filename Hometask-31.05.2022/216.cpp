d8b0964928b15656b4965e70cc634e9734162298
Ярослав Страхов <91416131+YarStr@users.noreply.github.com>
2022-05-31 20:30:43 +0500

    Create 216.cpp

---
+++ Hometask-31.05.2022/216.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    int n;
+    fin >> n;
+
+    int summ = 0;
+    int mx = 0;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        int value;
+        fin >> value;
+        summ += value;
+        mx = max(mx, value);
+    }
+
+    fout << min(summ / 2, summ - mx);
+
+    return 0;
+}
